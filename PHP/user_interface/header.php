f4ea10984dd92bb546dccec51151053683213d63
mayet <mayet.Tsiry@hotmail.com>
2022-09-18 00:29:22 +0200

    ajout d'un fichier qui contiendra le champ permettant d'effectuer une recherche avancée et dun fichier qui servira a afficher un ensemble de résultat suite à une recherche sur un film

---
+++ PHP/user_interface/header.php
@@ -0,0 +1,9 @@
+
+
+<ul>
+  <li><a href="add_update.php?action='add'">Ajouter un film</a></li>
+  <li><a href="">Recherche Avancée</a></li>
+  <form method="get" action="research.php">
+  <li><input type="text" name="rechercheRapide"><input type="submit" name="go"></li>
+  </form>
+</ul>
