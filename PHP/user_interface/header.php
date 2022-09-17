57bd6493447ed25ada8e04f232e53f7bf986dfa9
MAYET Tsiry <mayet.tsiry@hotmail.com>
2022-09-18 00:30:55 +0200

    ajout du git ignore

--- PHP/user_interface/header.php
+++ PHP/user_interface/header.php
@@ -7,8 +7,8 @@ function headerShow()
     <li class = 'elem_list_header'><a class = 'contenu_elem_liste_header lien_header' href ='home.php'><div class = 'animation_header'>Accueil</div></a></li>
     <li class = 'elem_list_header'><a class = 'contenu_elem_liste_header lien_header' href='add_update.php?action=add'><div class = 'animation_header'>Ajouter un film</div></a></li>
     <li class = 'elem_list_header'><a class = 'contenu_elem_liste_header lien_header' href='advanced_research.php'><div class = 'animation_header'>Recherche Avancée</div></a></li>
-    <li class = 'elem_list_header'><a class = 'contenu_elem_liste_header lien_header' href='acteurs_search.php'><div class = 'animation_header'>Rechercher un film par acteur</div></a></li>
-    <li class = 'elem_list_header'><input id = 'search_bar' class = 'contenu_elem_liste_header' type='text' name='keyResearch' placeholder = 'Recherche...'><input id = 'search_button' type='submit' value=''></li>
+    <li class = 'elem_list_header'><a class = 'contenu_elem_liste_header lien_header' href='acteurs_search.php'><div class = 'animation_header'>Films par acteur</div></a></li>
+    <li class = 'elem_list_header'><input id = 'search_bar' class = 'contenu_elem_liste_header' type='text' name='keyResearch' placeholder = 'Recherche...' autocomplete = 'off'><div id = 'proposition_recherche'></div><input id = 'search_button' type='submit' value=''></li>
   </ul>
   </form>";
 }
