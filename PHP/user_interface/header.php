f680fdb1700b2347bb2498af6b6bc5e7a69f1306
mayet <mayet.Tsiry@hotmail.com>
2022-09-18 00:29:22 +0200

    header.php envoit bien vers les 3 pages différents avec des paramètres à prendre en compte

--- PHP/user_interface/header.php
+++ PHP/user_interface/header.php
@@ -1,9 +1,7 @@
-
-
 <ul>
   <li><a href="add_update.php?action='add'">Ajouter un film</a></li>
-  <li><a href="">Recherche Avancée</a></li>
-  <form method="get" action="research.php">
-  <li><input type="text" name="rechercheRapide"><input type="submit" name="go"></li>
+  <li><a href="advanced_research">Recherche Avancée</a></li>
+  <form method="get" action="display_research.php">
+  <li><input type="text" name="rechercheRapide"><input type="submit" value="Go"></li>
   </form>
 </ul>
