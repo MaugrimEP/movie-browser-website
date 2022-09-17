cd26a0d7ce3ff1b2752789ec958f304b4d7835bd
mayet <mayet.Tsiry@hotmail.com>
2022-09-18 00:29:24 +0200

    le code du home passe au W3C validator

--- PHP/user_interface/header.php
+++ PHP/user_interface/header.php
@@ -2,14 +2,13 @@
 function headerShow()
 {
   echo "
+  <form method='get' action='display_research.php'>
+  <input type='hidden' name='typeRecherche' value='fast'/>
   <ul>
     <li><a href='add_update.php?action=add'>Ajouter un film</a></li>
     <li><a href='advanced_research.php'>Recherche Avancée</a></li>
-
-    <form method='get' action='display_research.php'>
     <li><input type='text' name='keyResearch'><input type='submit' value='Search'></li>
-    <input type='hidden' name='typeRecherche' value='fast'/>
-    </form>
-  </ul>";
+  </ul>
+  </form>";
 }
 ?>
--- PHP/user_interface/home.php
+++ PHP/user_interface/home.php
@@ -4,6 +4,20 @@ require_once('../functions/functions.php');
 require_once('header.php');
 $db=new BD('../class/base_stock/database');
 $rows=$db->getTenFirstRows();
-headerShow();
-displayHome($rows);
+
 ?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Accueil</title>
+</head>
+<body>
+  <header><?php headerShow();?></header>
+  <article>
+  <h2>Les 10 derniers films en date</h2>
+  <?php displayHome($rows);?>
+  </article>
+</body>
+</html>
