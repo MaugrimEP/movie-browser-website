7d3a25f00e9f2fdb63ebf2b0d707fff425633583
Julien SOYSOUVANH <julien.soysouvanh@etu.univ-orleans.fr>
2022-09-18 00:29:26 +0200

    Inclusion du header et ajout d'un h1 dépendant de ['action'] : Ajout d'un film ou édition d'un film

--- PHP/user_interface/add_update.php
+++ PHP/user_interface/add_update.php
@@ -1,16 +1,25 @@
-<?php
-//variable par méthode GET : [idFilm=l'id du film] et action = <add|update> require_once('header.php')
-?>
+<!--
+variable par méthode GET : [idFilm=l'id du film] et action = <add|update> require_once('header.php')
+-->
+
+<?php require_once header.php ?>
+
 <!doctype html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8">
-    <title>Recherche</title>
-  </head>
-<body>
-  <header></header>
-  <article>
-    <h2></h2>
-  </article>
-</body>
+	<head>
+		<meta charset="utf-8">
+		<title>Ajout d'un film</title>
+	</head>
+	<body>
+		<header>
+			<?php headerShow() ?>
+		</header>
+		<?php if $_GET['action']=="add"{
+			echo "<h1>Ajouter un film</h1>";
+		}
+		else{
+			echo "<h1>Editer un film</h1>";
+		} ?>
+	</body>
 </html>
+
